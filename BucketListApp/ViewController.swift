3b8d5146d433a8f37ee826d61c1fb21dfaa294d2
pervezn <nidapervez2020@u.northwestern.edu>
2017-07-09 22:46:09 -0500

    Initial Commit

---
+++ BucketListApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BucketListApp
+//
+//  Created by Nida Pervez on 7/9/17.
+//  Copyright © 2017 Nida Pervez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
