989420e58a4f5318c09daac879adee737eb4b0f7
pervezn <nidapervez2020@u.northwestern.edu>
2017-07-12 11:21:57 -0500

    Added the map and set up firebase

--- BucketListApp/ViewControllers/MapPageViewController.swift
+++ BucketListApp/ViewControllers/MapPageViewController.swift
@@ -7,15 +7,23 @@
 //
 
 import UIKit
+import MapKit
 
 class MapPageViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let initialLocation = CLLocation(latitude: 41.888543, longitude: -87.635444) //change this to the person's initial location
+        centerMapOnLocation(location: initialLocation)
     }
-
+    let regionRadius: CLLocationDistance = 1000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
